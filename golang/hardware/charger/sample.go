10c5ffb757ef92142e2f6170ce5d6e2a06e1c88e
linqiankai <417350372@qq.com>
2021-12-01 11:55:56 +0800

    将订单置成接口

--- golang/hardware/charger/sample.go
+++ golang/hardware/charger/sample.go
@@ -1,1 +1,108 @@
 package charger
+
+type Sample interface {
+	SetElectricity(value float64)
+	SetPower(value float64)
+	SetVoltageA(value float64)
+	SetVoltageB(value float64)
+	SetVoltageC(value float64)
+	SetCurrentA(value float64)
+	SetCurrentB(value float64)
+	SetCurrentC(value float64)
+}
+
+func (x *StartTransactionReq) SetElectricity(value float64) {
+	x.MeterStart = value
+}
+
+func (x *StartTransactionReq) SetPower(value float64) {
+	x.Power = value
+}
+
+func (x *StartTransactionReq) SetVoltageA(value float64) {
+	x.VoltageA = value
+}
+
+func (x *StartTransactionReq) SetVoltageB(value float64) {
+	x.VoltageB = value
+}
+
+func (x *StartTransactionReq) SetVoltageC(value float64) {
+	x.VoltageC = value
+}
+
+func (x *StartTransactionReq) SetCurrentA(value float64) {
+	x.CurrentA = value
+}
+
+func (x *StartTransactionReq) SetCurrentB(value float64) {
+	x.CurrentB = value
+}
+
+func (x *StartTransactionReq) SetCurrentC(value float64) {
+	x.CurrentC = value
+}
+
+func (x *StopTransactionReq) SetElectricity(value float64) {
+	x.MeterStop = value
+}
+
+func (x *StopTransactionReq) SetPower(value float64) {
+	x.Power = value
+}
+
+func (x *StopTransactionReq) SetVoltageA(value float64) {
+	x.VoltageA = value
+}
+
+func (x *StopTransactionReq) SetVoltageB(value float64) {
+	x.VoltageB = value
+}
+
+func (x *StopTransactionReq) SetVoltageC(value float64) {
+	x.VoltageC = value
+}
+
+func (x *StopTransactionReq) SetCurrentA(value float64) {
+	x.CurrentA = value
+}
+
+func (x *StopTransactionReq) SetCurrentB(value float64) {
+	x.CurrentB = value
+}
+
+func (x *StopTransactionReq) SetCurrentC(value float64) {
+	x.CurrentC = value
+}
+
+func (x *ChargingInfoReq) SetElectricity(value float64) {
+	x.Electricity = value
+}
+
+func (x *ChargingInfoReq) SetPower(value float64) {
+	x.Power = value
+}
+
+func (x *ChargingInfoReq) SetVoltageA(value float64) {
+	x.VoltageA = value
+}
+
+func (x *ChargingInfoReq) SetVoltageB(value float64) {
+	x.VoltageB = value
+}
+
+func (x *ChargingInfoReq) SetVoltageC(value float64) {
+	x.VoltageC = value
+}
+
+func (x *ChargingInfoReq) SetCurrentA(value float64) {
+	x.CurrentA = value
+}
+
+func (x *ChargingInfoReq) SetCurrentB(value float64) {
+	x.CurrentB = value
+}
+
+func (x *ChargingInfoReq) SetCurrentC(value float64) {
+	x.CurrentC = value
+}
